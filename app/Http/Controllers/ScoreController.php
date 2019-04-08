ae94c4933747b3ba54d9c26cda9bf3c39705c40c
Probe31 <bhpaucar@gmail.com>
2019-04-08 12:53:59 -0500

    huevitus

---
+++ app/Http/Controllers/ScoreController.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Score;
+
+class ScoreController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'username'=>'required',
+            'score_ms'=>'required',
+
+        ]);
+        
+        $score = new Score([
+            'name' => $request->get('username'),
+            'score' => $request->get('score_ms'),
+
+        ]);
+        
+        $score->save();
+        return response()->json([
+            'result' => 'OK'
+        ]); 
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+    public function getBestScore()
+    {
+        $user = Score::query()->orderBy("score")->take(1)->get();
+
+
+        return response()->json([
+            'username' => $user[0]->name,
+            'score_ms' => $user[0]->score
+        ]); 
+    }
+}
